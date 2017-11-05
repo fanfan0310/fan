e7dee68f5d2d26bccf699bf31fcdbc8c21766d07
fanyifan <1165863766@qq.com>
2017-11-05 20:32:28 +0800

    1

---
+++ aa.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2017-11-03 19:34:31
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-11-03 19:35:02
+*/
+
+const name = 'zhang'
+for(let i = 0;i<5;i++){
+    console.log(i)
+}
